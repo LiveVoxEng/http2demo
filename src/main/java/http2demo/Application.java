abea2378b0f30d8bf93ca49824c14ce4ac26e75f
Aaron Meriwether <me@ameriwether.com>
2024-04-05 15:38:03 -0400

    Initial commit

---
+++ src/main/java/http2demo/Application.java
@@ -0,0 +1,38 @@
+package http2demo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletException;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/upload")
+    public static class FileUploadController {
+
+        @PostMapping
+        public String post(
+            @RequestParam("file") MultipartFile file
+        ) throws ServletException, IOException {
+            IOUtils.copyLarge(file.getInputStream(), OutputStream.nullOutputStream());
+            return "Received file named " + file.getOriginalFilename() + " of size " + file.getSize() + "\n";
+        }
+
+    }
+
+}
